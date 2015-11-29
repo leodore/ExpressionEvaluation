634ff1c9c6fc8d4046bd77ccd0369b7eab035b09
leodore <lemuellly@gmail.com>
2015-11-29 10:05:47 +0800

    Update EvaluateSuffixExpressionTest.java

--- EvaluateSuffixExpressionTest.java
+++ EvaluateSuffixExpressionTest.java
@@ -2,10 +2,12 @@ public class EvaluateSuffixExpressionTest {
   public static void main(String[] args) {
   		System.out.println("Enter an arithmetic expression(supports + - * / operations):");
   		
-  		Scanner in = new Scanner(System.in);
-  		String expression = in.nextLine();
-  		in.close();		
-  		double result = evaluateExpression(transferToSuffixExpression(expression));
-  		System.out.println("The result of the expression is: " + result);
+		Scanner in = new Scanner(System.in);
+		String expression = in.nextLine();
+		in.close();	
+		
+		double result = EvaluateSuffixExpression.evaluateExpression(
+				EvaluateSuffixExpression.transferToSuffixExpression(expression));
+		System.out.println("The result of the expression is: " + result);
   	}
-	}
+}
