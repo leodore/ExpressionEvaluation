ab0c8dea9957696d4f58727a1cf49640145df545
leodore <lemuellly@gmail.com>
2015-11-29 09:27:10 +0800

    ExpressionEvaluationTest.java

---
+++ ExpressionEvaluationTest.java
@@ -0,0 +1,11 @@
+public class ExpressionEvaluationTest {
+  public static void main(String[] args) {
+  		System.out.println("Enter an arithmetic expression(supports + - * / operations):");
+  		
+  		Scanner in = new Scanner(System.in);
+  		String expression = in.nextLine();
+  		in.close();		
+  		double result = evaluateExpression(transferToSuffixExpression(expression));
+  		System.out.println("The result of the expression is: " + result);
+  	}
+	}
